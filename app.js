a265decafcc3e4feb759b75f1d9a406300bca276
Bruno Campos <bruno.campos@21mobile.com>
2016-08-08 17:37:06 -0300

    rolou

--- app.js
+++ app.js
@@ -429,7 +429,7 @@ function getDBmessages(senderID){
 
      console.log("saved messages");
 
-     Message.find({}).sort([["order", "ascending"]]).exec(function(err, docs){
+     Message.find({}).sort([["order", 1]]).exec(function(err, docs){
 
        //console.log("Find some messages", docs);
        //console.log("errors", err);
@@ -450,7 +450,7 @@ function getDBmessages(senderID){
           console.log("json object", messagejson);
           setTimeout(function(){
               callSendAPI(messagejson);
-          }, 500)
+          }, 1000)
 
         });
 
