f61337636d900ae81e520c06ec2c75096018209c
Bruno Campos <bruno.campos@21mobile.com>
2016-08-08 09:50:56 -0300

    teste

--- app.js
+++ app.js
@@ -431,7 +431,7 @@ function getDBmessages(){
 
      Message.find({}).exec(function (err, docs){
 
-
+        if(err) throw err;
 
         console.log("Find some messages", docs);
 
@@ -440,17 +440,25 @@ function getDBmessages(){
           arrayMessages.push(doc);
         });
 
-        mongoose.connection.db.close(function (err) {
-          if(err) throw err;
+        // Since this is an example, we'll clean up after ourselves.
+       mongoose.connection.db.collection('messages').drop(function (err) {
+         if(err) throw err;
 
-          console.log("Closed Connection");
-        });
+         console.log("Drop Messages");
 
+         // Only close the connection when your app is terminating
+         mongoose.connection.db.close(function (err) {
+           if(err) throw err;
+
+           console.log("Closed Connection");
+         });
+       });
 
      })
 
   });
 
+  return arrayMessages;
 
 }
 
