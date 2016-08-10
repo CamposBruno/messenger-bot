153cb6167f6e279faf8d8d18fb91a751a23b353b
Bruno Campos <bruno.campos@21mobile.com>
2016-08-10 20:09:59 -0300

    alteração pesada, vamos la

--- app.js
+++ app.js
@@ -202,7 +202,7 @@ app.post('/webhook', function (req, res) {
             throw err;
           }
 
-          Message.find({"reference" : "START_BOT_T", "mismatch" : false}).sort({"order": 1}).exec(function(err, docs){
+          Message.find({"reference" : "START_BOT_T", "mismatch" : {$not: true}}).sort({"order": 1}).exec(function(err, docs){
             console.log("DEBUG: busca mensagens com payload enviado. achou: " + docs.length);
 
             //console.log("errors", err);
