2ebd5cccec6f032ec86269185f77d12aee4d4248
Bruno Campos <bruno.campos@21mobile.com>
2016-08-18 11:34:20 -0300

    vamos la

--- app.js
+++ app.js
@@ -526,7 +526,7 @@ function enviarMensagem(senderID, messagejson, message, index){
   newUserSession.save();
 
   console.log("DEBUG: MESSAGE ", message);
-  var timeout = message.timeout ? message.timeout : 3000;
+  var timeout = message.tempo ? message.tempo : 3000;
 
   console.log("timeout : " + timeout);
 
