af70b661ac287a6bd0a1a424253eae82dc00b685
Bruno Campos <bruno.campos@21mobile.com>
2016-10-04 16:23:01 -0300

    teste

--- app.js
+++ app.js
@@ -1213,6 +1213,7 @@ function buscaMsgIdleEnvia(where, set, idle){
                 message: JSON.parse(text)
               };
               console.log("DEBUG: envia mensagem IDLE para usuario : " + doc.sender_id);
+              currentUser.user_id = doc.sender_id;
               enviarMensagem(currentUser, messagejson, {tempo: null, reference : null}, 1);
               //callSendAPI(messagejson);
 
